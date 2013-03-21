55a0d864e313a655f8bdfe8e02c313779c572e36
Denilson Telaroli <ddts80@gmail.com>
2013-03-21 10:43:29 -0300

    Correção de bug de ambientes diferentes em NumberUtil

--- src/br/com/flexait/gateway/util/NumberUtil.java
+++ src/br/com/flexait/gateway/util/NumberUtil.java
@@ -8,7 +8,7 @@ public class NumberUtil {
 		DecimalFormat df = new DecimalFormat("#.00");
 		
 		String format = df.format(value);
-		return format.replace(".", "").replaceFirst("^0+", "");
+		return format.replace(".", "").replace(",", "").replaceFirst("^0+", "");
 	}
 
 	public static double parse(String value) {
